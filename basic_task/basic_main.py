d4cf73d122b9f23843012a4ff593307e0a1f6483
riyaad-mangera <149512297+riyaad-mangera@users.noreply.github.com>
2024-03-03 15:17:00 +0000

    Add files via upload

--- basic_task/basic_main.py
+++ basic_task/basic_main.py
@@ -41,6 +41,10 @@ def check_border(state, change):
     new_state = state + change
     if (new_state < 0) or (new_state > grid_states-1):
         return state
+    elif (change == -1 and state % grid_x == 0) or (change == 1 and state % grid_x == (grid_x - 1)):
+        return state
+    elif new_state in nan_states:
+        return state
     else:
         return new_state
 
@@ -75,55 +79,81 @@ def check_rails(state):
 def generate_init_r_matrix(nan_r):
     # 0 = up, 1 = right, 2 = down, 3 = left, 4 = mine
     for state in all_states:
-        for action in all_actions:
-            match action:
-                case 0:
-                    border_check = check_border(mine_map(state), -grid_x)
-                    if border_check != state:
-                        rail_check = check_rails(border_check)
-                        new_states = check_ore(rail_check)
-                    else:
-                        new_states = border_check
-                case 1:
-                    border_check = check_border(mine_map(state), 1)
-                    if border_check != state:
-                        rail_check = check_rails(border_check)
-                        new_states = check_ore(rail_check)
-                    else:
-                        new_states = border_check
-                case 2:
-                    border_check = check_border(mine_map(state), grid_x)
-                    if border_check != state:
-                        rail_check = check_rails(border_check)
-                        new_states = check_ore(rail_check)
-                    else:
-                        new_states = border_check
-                case 3:
-                    border_check = check_border(mine_map(state), -1)
-                    if border_check != state:
-                        rail_check = check_rails(border_check)
-                        new_states = check_ore(rail_check)
-                    else:
-                        new_states = border_check
-                case 4:
-                    new_states = mine_action_map(state)
+        if (state not in nan_states):
+            for action in all_actions:
+                match action:
+                    case 0:
+                        border_check = check_border(mine_map(state), -grid_x)
+                        if border_check != state:
+                            rail_check = check_rails(border_check)
+                            new_states = check_ore(rail_check)
+                        else:
+                            new_states = border_check
+                    case 1:
+                        border_check = check_border(mine_map(state), 1)
+                        if border_check != state:
+                            rail_check = check_rails(border_check)
+                            new_states = check_ore(rail_check)
+                        else:
+                            new_states = border_check
+                    case 2:
+                        border_check = check_border(mine_map(state), grid_x)
+                        if border_check != state:
+                            rail_check = check_rails(border_check)
+                            new_states = check_ore(rail_check)
+                        else:
+                            new_states = border_check
+                    case 3:
+                        border_check = check_border(mine_map(state), -1)
+                        if border_check != state:
+                            rail_check = check_rails(border_check)
+                            new_states = check_ore(rail_check)
+                        else:
+                            new_states = border_check
+                    case 4:
+                        new_states = mine_action_map(state)
 
-            if type(new_states) == list:
-                for new_state in new_states:
-                    if new_state != state:
-                        nan_r[state, new_state] = 0
+                if type(new_states) == list:
+                    for new_state in new_states:
+                        if new_state != state:
+                            nan_r[state, new_state] = 0
+                        else:
+                            continue
+                else:
+                    if new_states != state:
+                        nan_r[state, new_states] = 0
                     else:
                         continue
-            else:
-                if new_states != state:
-                    nan_r[state, new_states] = 0
-                else:
-                    continue
-
+        else:
+            continue
+        
     return nan_r
 
 R_matrix = generate_init_r_matrix(R_matrix)
 #print(R_matrix)
 
+f = open('debug.txt', 'w')
+
 for index, state in enumerate(R_matrix):
-    print(f'from state: {index}\n{state}')
+    f.write(f'\nfrom state: {index}')
+
+    i = 0
+    while i < grid_states:
+        f.write(f'\n{np.array2string(state[i:i + grid_x])}')
+        i+= grid_x
+        
+
+    f.write(f'{np.array2string(state[grid_states: grid_states + len(diamond_ore_states)])}\n')
+
+f.close()
+
+
+#########################################################
+    
+starting_state = convert_pos_to_state([[7, 3]])
+
+print(f'Starting State: {starting_state}')
+print(f'Current Matrix: {R_matrix[starting_state]}')
+
+valid_actions = np.where(~np.isnan(R_matrix[starting_state]))[1]
+print(f'Valid Actions: {valid_actions}')
